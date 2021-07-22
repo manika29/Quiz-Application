d42e10977091d0a8d8fa9b29f39a846f020b62fb
Manika Sharma <manika.sharma1964@gmail.com>
2021-07-22 17:00:26 +0530

    used states and context api

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,37 @@
+export const Questions = [
+  {
+    prompt: "1 - You can have ice cream ___ you finish your dinner.",
+    optionA: "when",
+    optionB: "but",
+    optionC: "and",
+    optionD: "or",
+    optionE: "while",
+    answer: "optionE",
+  },
+
+  {
+    prompt: "2 - The milk is ___ the refrigerator",
+    optionA: "to",
+    optionB: "of",
+    optionC: "at",
+    optionD: "in",
+    optionE: "from",
+    answer: "optionD",
+  },
+
+  {
+    prompt: "3 - The world is ___ of people from all strata of society",
+    optionA: "made in",
+    optionB: "made up",
+    optionC: "made by",
+    answer: "optionB",
+  },
+
+  {
+    prompt: "4 - The earth ___ in its own axis for a very long time",
+    optionA: "has been rotating",
+    optionB: "has rotated",
+    optionC: "have been rotating",
+    answer: "optionA",
+  },
+];
